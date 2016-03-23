57430202b650bfc09f60d316e2051da93c097895
SteamyThePunk <BenjaminDavidAndersen@gmail.com>
2016-03-23 10:08:55 -0400

    Simple Blueprint Library
    Much cleaner, but I may want to still put the TransientTexture and
    rendering mess into it's own class...

---
+++ Source/OpenCV/Private/OpenCVBPLibrary.cpp
@@ -0,0 +1,87 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OpenCV.h"
+#include "OpenCVBPLibrary.h"
+
+void UOpenCVBPLibrary::OpenCam(FVideoCapture videoCapture, FVideoCapture & videoCaptureOut, int32 CameraID, bool &isOpen)
+{
+	isOpen = videoCapture.cvVideoCapture.open(CameraID);
+	videoCaptureOut = videoCapture;
+}
+
+void UOpenCVBPLibrary::IsOpened(FVideoCapture videoCapture, bool &isOpen)
+{
+	isOpen = videoCapture.cvVideoCapture.isOpened();
+}
+
+void UOpenCVBPLibrary::Capture(FMat mat, FMat & matOut, FVideoCapture videoCapture, bool &isCaptured)
+{
+	isCaptured = videoCapture.cvVideoCapture.read(mat.cvMat);
+	matOut = mat;
+}
+
+void UOpenCVBPLibrary::MatInfo(FMat mat, int32 &colsOut, int32 &rowsOut, int32 &stepOut, int32 &totalOut, int32 &elemSizeOut, int32 &channelsOut)
+{
+	colsOut = mat.cvMat.cols;
+	rowsOut = mat.cvMat.rows;
+	stepOut = mat.cvMat.step;
+	totalOut = mat.cvMat.total();
+	elemSizeOut = mat.cvMat.elemSize();
+	channelsOut = mat.cvMat.channels();
+}
+
+// we need a transient texture for the RenderToTexture function
+void UOpenCVBPLibrary::CreateTransient(int32 width, int32 height, UTexture2D * Texture)
+{
+	Texture = UTexture2D::CreateTransient(width, height);
+}
+
+void UOpenCVBPLibrary::RenderToTexture(FMat mat, UTexture2D * Texture)
+{
+	if (Texture->Resource)
+	{
+		struct FUpdateTextureRegionsData
+		{
+			FTexture2DResource* Texture2DResource;
+			int32 MipIndex;
+			uint32 NumRegions;
+			FUpdateTextureRegion2D* Regions;
+			uint32 SrcPitch;
+			uint32 SrcBpp;
+			uint8* SrcData;
+		};
+
+		FUpdateTextureRegionsData* RegionData = new FUpdateTextureRegionsData;
+
+		RegionData->Texture2DResource = (FTexture2DResource*)Texture->Resource;
+		RegionData->MipIndex = 0;
+		RegionData->NumRegions = mat.cvMat.channels();
+		RegionData->Regions = new FUpdateTextureRegion2D(0, 0, 0, 0, mat.cvMat.size().width, mat.cvMat.size().height);
+		RegionData->SrcPitch = (uint32)(mat.cvMat.elemSize() * mat.cvMat.size().width);
+		RegionData->SrcBpp = (uint32)mat.cvMat.elemSize();
+		RegionData->SrcData = (uint8*)mat.cvMat.data;
+
+		ENQUEUE_UNIQUE_RENDER_COMMAND_ONEPARAMETER(
+			UpdateTextureRegionsData,
+			FUpdateTextureRegionsData*, RegionData, RegionData,
+			{
+				for (uint32 RegionIndex = 0; RegionIndex < RegionData->NumRegions; ++RegionIndex)
+				{
+					int32 CurrentFirstMip = RegionData->Texture2DResource->GetCurrentFirstMip();
+					if (RegionData->MipIndex >= CurrentFirstMip)
+					{
+						RHIUpdateTexture2D(
+							RegionData->Texture2DResource->GetTexture2DRHI(),
+							RegionData->MipIndex - CurrentFirstMip,
+							RegionData->Regions[RegionIndex],
+							RegionData->SrcPitch,
+							RegionData->SrcData
+							+ RegionData->Regions[RegionIndex].SrcY * RegionData->SrcPitch
+							+ RegionData->Regions[RegionIndex].SrcX * RegionData->SrcBpp
+							);
+					}
+				}
+				delete RegionData;
+			});
+	}
+}
---
+++ Source/OpenCV/Public/OpenCVBPLibrary.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Mat.h"
+#include "VideoCapture.h"
+#include "OpenCVBPLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OPENCV_API UOpenCVBPLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, Category = "OpenCV,VideoIO")
+		static void OpenCam(FVideoCapture videoCapture, FVideoCapture & videoCaptureOut, int32 CameraID, bool &isOpen);
+
+	UFUNCTION(BlueprintPure, Category = "OpenCV,VideoIO")
+		static void IsOpened(FVideoCapture videoCapture, bool &isOpen);
+
+	UFUNCTION(BlueprintCallable, Category = "OpenCV,VideoIO")
+		static void Capture(FMat mat, FMat & matOut, FVideoCapture videoCapture, bool &isCaptured);
+
+	UFUNCTION(BlueprintPure, Category = "OpenCV,Mat")
+		static void MatInfo(FMat mat, int32 &colsOut, int32 &rowsOut, int32 &stepOut, int32 &totalOut, int32 &elemSizeOut, int32 &channelsOut);
+
+	UFUNCTION(BlueprintCallable, Category = "OpenCV,Tools")
+		static void CreateTransient(int32 width, int32 height, UTexture2D * Texture);
+
+	UFUNCTION(BlueprintCallable, Category = "OpenCV,Mat")
+		static void RenderToTexture(FMat mat, UTexture2D * Texture);
+};
